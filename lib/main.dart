ea0979b5bc825f223ac1a33102e63c0326a34133
SeoDongSeuk <cast3613@gmail.com>
2022-01-03 22:58:52 +0900

    Merge branch 'main' of https://github.com/SeoDongSeuk/AppDevelopment

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+<<<<<<< HEAD
         centerTitle: true,
         title: Text('Englsih App'),
         backgroundColor: Colors.white,
@@ -36,6 +37,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // shape 둥굴게 하기
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.vertical(bottom: Radius.circular(16))),
+=======
+        title: Text(
+          'Englsih App111111',
+        ),
+        backgroundColor: Colors.blue,
+        titleTextStyle: TextStyle(color: Colors.black),
+>>>>>>> caee8fddbb0c445e4acfb49b9eca00d478263a6c
       ),
       body: Container(child: Text('Test')),
     );
